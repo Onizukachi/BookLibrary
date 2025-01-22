b71b5e3cfaf89275902ca45e59c1ee970f063de5
Onizukachi <dark_sao@mail.ru>
2025-01-22 13:08:41 +0300

    add filter by price to products

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,8 @@ class CategoriesController < ApplicationController
   before_action :set_category, only: [:show]
   def show
     @products = @category.products
+    @products = @products.where("price >= ?", params[:min].to_i) if params[:min].present?
+    @products = @products.where("price <= ?", params[:max].to_i) if params[:max].present?
   end
 
   private
